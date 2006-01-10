f7c9ae9bc0cb66ac39b9466adfee170654833f79
Marco Vassura <marco@apache.org>
2006-01-10 10:02:00 +0000

    Base classes and tests for porting log4php in php5.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4php/trunk@367546 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/php5/tests/AllTests.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Copyright 1999,2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @category   tests   
+ * @package    log4php
+ * @author     Marco Vassura
+ * @license    http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @version    SVN: $Id$
+ * @link       http://logging.apache.org/log4php
+ */
+
+/** */
+if (!defined('PHPUnit2_MAIN_METHOD')) {
+    define('PHPUnit2_MAIN_METHOD', 'AllTests::main');
+}
+if (!defined('LOG4PHP_DIR')) {
+    define('LOG4PHP_DIR', '../log4php');
+}
+
+if (!defined('LOG4PHP_DEFAULT_INIT_OVERRIDE')) {
+    define('LOG4PHP_DEFAULT_INIT_OVERRIDE', true);
+}
+
+require_once('PHPUnit2/Framework/TestSuite.php');
+require_once('PHPUnit2/TextUI/TestRunner.php');
+
+require_once('LoggerLogTestCase.php');
+require_once('LoggerLevelTestCase.php');
+require_once('LoggerRootTestCase.php');
+require_once('LoggerBasicConfiguratorTestCase.php');
+
+class AllTests {
+
+    public static function main() {
+        PHPUnit2_TextUI_TestRunner::run(self::suite());
+    }
+
+    public static function suite() {
+        $suite = new PHPUnit2_Framework_TestSuite('Log4php5');
+
+        $suite->addTestSuite('LoggerLogTestCase');
+        $suite->addTestSuite('LoggerLevelTestCase');
+        $suite->addTestSuite('LoggerRootTestCase');
+        
+        $suite->addTestSuite('LoggerBasicConfiguratorTestCase');
+
+        return $suite;
+    }
+}
+
+if (PHPUnit2_MAIN_METHOD == 'AllTests::main') {
+    AllTests::main();
+}
+?>
---
+++ src/php5/tests/LoggerBasicConfiguratorTestCase.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Copyright 1999,2004 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @category   tests
+ * @package    log4php
+ * @author     Marco Vassura
+ * @license    http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @version    SVN: $Id$
+ * @link       http://logging.apache.org/log4php
+ */
+
+/**  */
+require_once 'PHPUnit2/Framework/TestCase.php';
+require_once LOG4PHP_DIR . '/LoggerBasicConfigurator.php';
+
+class LoggerBasicConfiguratorTestCase extends PHPUnit2_Framework_TestCase {
+	
+	private $configurator;
+	
+	protected function setup() {
+		LoggerBasicConfigurator::configure();
+	}
+	
+	public function testRootLogger() {
+		$root = LoggerManager::getRootLogger();
+		
+		$a = $root->getAppender('A1');
+		$this->assertTrue($a instanceof LoggerAppenderConsole);
+		
+		$l = $a->getLayout();
+		$this->assertTrue($l instanceof LoggerLayoutTTCC);
+		
+	}
+
+}
+?>
