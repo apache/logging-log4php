27de763d8ad733446e2f5115b924c45b83990f43
Christian Grobmeier <grobmeier@apache.org>
2009-05-22 11:38:06 +0000

    renamed to LoggerLayoutPattern (conforms to the other layouts now)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/log4php/trunk@777487 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/php/appenders/LoggerAppenderAdodb.php
+++ src/main/php/appenders/LoggerAppenderAdodb.php
@@ -128,7 +128,7 @@ class LoggerAppenderAdodb extends LoggerAppender {
           return;
         }
         
-        $this->layout = LoggerLayout::factory('LoggerPatternLayout');
+        $this->layout = LoggerLayout::factory('LoggerLayoutPattern');
         $this->layout->setConversionPattern($this->getSql());
     
         // test if log table exists
