580f10fef984fbfe04e787118da064861185f294
Christian Grobmeier <grobmeier@apache.org>
2009-06-16 18:33:13 +0000

    reenabled teardown method
    git-svn-id: https://svn.apache.org/repos/asf/incubator/log4php/trunk@785336 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/php/configurators/LoggerConfiguratorIniTest.php
+++ src/test/php/configurators/LoggerConfiguratorIniTest.php
@@ -29,7 +29,7 @@ class LoggerConfiguratorIniTest extends PHPUnit_Framework_TestCase {
 		
 	}
 	
-	protected function xtearDown() {
+	protected function tearDown() {
 		LoggerManager::resetConfiguration();
 	}
         
