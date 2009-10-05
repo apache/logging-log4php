02336c8e52c2433efc85adf96094a5c04f42dfaa
Christian Hammers <chammers@apache.org>
2009-10-05 21:08:41 +0000

    Example for appender_dailyfile
    git-svn-id: https://svn.apache.org/repos/asf/incubator/log4php/branches/chammers@822011 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/examples/php/appender_dailyfile.php
@@ -0,0 +1,8 @@
+<?php
+// START SNIPPET: doxia
+require_once dirname(__FILE__).'/../../main/php/Logger.php';
+Logger::configure(dirname(__FILE__).'/../resources/appender_dailyfile.properties');
+
+$logger = Logger::getRootLogger();
+$logger->debug("Hello World!");
+// END SNIPPET: doxia
