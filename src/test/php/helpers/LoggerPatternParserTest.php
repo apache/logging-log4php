f2ca21c3db2d6abc51a6f42a763ae17cb84fe4aa
Christian Hammers <chammers@apache.org>
2009-10-05 22:06:35 +0000

    Corrected phpdoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/log4php/branches/chammers@822047 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/php/helpers/LoggerPatternParserTest.php
+++ src/test/php/helpers/LoggerPatternParserTest.php
@@ -17,7 +17,7 @@
  * 
  * @category   tests   
  * @package    log4php
- * @subpackage appenders
+ * @subpackage helpers
  * @license    http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  * @version    SVN: $Id$
  * @link       http://logging.apache.org/log4php
