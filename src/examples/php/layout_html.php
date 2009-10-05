6395613de5850b77f60fecf39417249de4e9a9dd
Christian Hammers <chammers@apache.org>
2009-10-05 22:42:39 +0000

    Example for layout_html
    git-svn-id: https://svn.apache.org/repos/asf/incubator/log4php/branches/chammers@822066 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/examples/php/layout_html.php
@@ -0,0 +1,8 @@
+<?php
+// START SNIPPET: doxia
+require_once dirname(__FILE__).'/../../main/php/Logger.php';
+
+Logger::configure(dirname(__FILE__).'/../resources/layout_html.properties');
+$logger = Logger::getRootLogger();
+$logger->info("Hello World!");
+// END SNIPPET: doxia
