6ae8a4a077a8aebb1d8a8d804a97173edf5cec27
Christian Hammers <chammers@apache.org>
2009-10-22 22:50:50 +0000

    Changed version number to 2.0.0-incubating as Christian G. suggested on the Wiki page "Log4php Releaseplan".
    git-svn-id: https://svn.apache.org/repos/asf/incubator/log4php/trunk@828888 13f79535-47bb-0310-9956-ffa450edef68

--- package-config.php
+++ package-config.php
@@ -17,14 +17,14 @@
  */
 
 // parse pom.xml to get version in sync
-preg_match("/<version>(.+?)(-SNAPSHOT)?<\/version>/", file_get_contents("../../pom.xml"), $pom_version);
+preg_match("/<version>(.+?)(-(\w+))?<\/version>/", file_get_contents("../../pom.xml"), $pom_version);
 
 $name = 'log4php';
 $summary = 'log4Php is a PHP port of log4j framework';
-$version = $pom_version[1];
+$version = $pom_version[1].(empty($pom_version[3]) ? '' : $pom_version[3]);
 $versionBuild = 'b1';
 $apiVersion = '2.0.0';
-$state = empty($pom_version[2]) ? 'stable' : 'snapshot';
+$state = empty($pom_version[3]) ? 'stable' : 'snapshot';
 $apiStability = 'stable';
 
 $description = <<<EOT
