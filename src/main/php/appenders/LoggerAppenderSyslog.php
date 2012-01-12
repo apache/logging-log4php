9fb60a1e758e1e1ee40c31c1749c3d023f5ff986
Ivan Habunek <ihabunek@apache.org>
2012-01-12 13:37:23 +0000

    Replaced internal conversion of log level to syslog level in LoggerAppenderSyslog, instead using equivalent syslog level defined in LoggerLevel.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4php/trunk@1230527 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/php/appenders/LoggerAppenderSyslog.php
+++ src/main/php/appenders/LoggerAppenderSyslog.php
@@ -128,16 +128,6 @@ class LoggerAppenderSyslog extends LoggerAppender {
 	 * @var int
 	 */
 	private $intOption;
-	
-	/** Maps log4php levels to equivalent syslog priorities. */
-	private $levelMap = array(
-		LoggerLevel::TRACE => LOG_DEBUG,
-		LoggerLevel::DEBUG => LOG_DEBUG,
-		LoggerLevel::INFO  => LOG_INFO,
-		LoggerLevel::WARN  => LOG_WARNING,
-		LoggerLevel::ERROR => LOG_ERR,
-		LoggerLevel::FATAL => LOG_ALERT,
-	);
 
 	/**
 	 * Sets the {@link $ident}.
@@ -268,14 +258,7 @@ class LoggerAppenderSyslog extends LoggerAppender {
 		if($this->overridePriority) {
 			return $this->intPriority;
 		}
-		
-		$int = $level->toInt();
-		
-		if (isset($this->levelMap[$int])) {
-			return $this->levelMap[$int];
-		} else {
-			return LOG_DEBUG;
-		}
+		return $level->getSyslogEquivalent();
 	}
 	
 	/** Parses a syslog option string and returns the correspodning int value. */
