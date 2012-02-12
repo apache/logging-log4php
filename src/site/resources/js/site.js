acfb1da5463cc5378111f8cf55a80d445da2c9bc
Ivan Habunek <ihabunek@apache.org>
2012-02-12 14:00:02 +0000

    Site update: * Updated Bootstrap to v2.0. * New design, without breadcrumbs and logos. * NOTICE and LICENCE modified to include Glyphicons Free package which is bundled with Bootstrap. * Various improvements to site and docs.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4php/trunk@1243254 13f79535-47bb-0310-9956-ffa450edef68

--- src/site/resources/js/site.js
+++ src/site/resources/js/site.js
@@ -16,7 +16,7 @@ limitations under the License.
 */
 
 /* Executed on page load. */
-function siteOnLoad() {
+$(document).ready(function() {
 
 	//
 	// This is a hack to enable google-code-prettify to work with maven.
@@ -32,36 +32,75 @@ function siteOnLoad() {
 	// This hack restores the class of all <pre> elements which are the child of 
 	// a <div class="prettyprint">.
 	//
-	elements = document.getElementsByTagName('pre');
-	count = elements.length;
-	for(i = 0; i < count; i++) {
-		var parentClass = elements[i].parentNode.className;
-		if (parentClass.indexOf('prettyprint') >= 0) {
-			elements[i].className = parentClass;
+	$('pre').each(function() {
+		var parent = $(this).parent();
+		
+		if (parent.hasClass('prettyprint')) {
+			parent.removeClass('prettyprint');
+			$(this).addClass('prettyprint');
 		}
-	}
+		
+		if (parent.hasClass('linenums')) {
+			parent.removeClass('linenums');
+			$(this).addClass('linenums');
+		}
+	})
+	
+	// Hack to add default visuals to tables
+	$('table').each(function() {
+		if ($(this).hasClass('bodyTable')) {
+			
+			// Remove border="1" which is added by maven
+			this.border = 0;
+			
+			// Add bootstrap table styling
+			$(this).addClass('table table-striped table-bordered');
+		}
+	});
+	
+	// Render tabs
+	$('.auto-tabs').each(function(groupid) {
+
+		// Find tab bar
+		$(this).find('ul').each(function() {
+
+			// Add styling
+			$(this).addClass('nav nav-tabs');
+
+			// Go tab bar items
+			$(this).find('li').each(function(itemid) {
+			
+				// Set first tab as active
+				if (itemid == 0) {
+					$(this).addClass('active');
+				}
+				
+				// Replace text with a link to tab contents
+				var name = $(this).html();
+				var link = $('<a>')
+					.attr('href', '#' + 'tab-' + groupid + '-' + itemid)
+					.attr('data-toggle', 'tab')
+					.html(name);
+				$(this).html(link);
+			});
+		});
+		
+		// Find tab contents
+		$(this).find('.tab-content .tab-pane').each(function(itemid) {
+			
+			// Set first tab as active
+			if (itemid == 0) {
+				$(this).addClass('active');
+			}
+			
+			// Set the tab id
+			$(this).attr('id', 'tab-' + groupid + '-' + itemid);
+		});
+	});
+	
+	// Make external links open in new tab
+	$('a.externalLink').attr('target', '_blank');
 	
 	// Trigger prettyprint
 	prettyPrint();
-	
-	$('.tabs').tabs();
-}
-
-/* ========================================================
- * bootstrap-tabs.js v1.4.0
- * http://twitter.github.com/bootstrap/javascript.html#tabs
- * ========================================================
- * Copyright 2011 Twitter, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ======================================================== */!function(a){function b(a,b){b.find("> .active").removeClass("active").find("> .dropdown-menu > .active").removeClass("active"),a.addClass("active"),a.parent(".dropdown-menu")&&a.closest("li.dropdown").addClass("active")}function c(c){var d=a(this),e=d.closest("ul:not(.dropdown-menu)"),f=d.attr("href"),g,h;if(/^#\w+/.test(f)){c.preventDefault();if(d.parent("li").hasClass("active"))return;g=e.find(".active a").last()[0],h=a(f),b(d.parent("li"),e),b(h,h.parent()),d.trigger({type:"change",relatedTarget:g})}}"use strict",a.fn.tabs=a.fn.pills=function(b){return this.each(function(){a(this).delegate(b||".tabs li > a, .pills > li > a","click",c)})},a(document).ready(function(){a("body").tabs("ul[data-tabs] li > a, ul[data-pills] > li > a")})}(window.jQuery||window.ender);
+});
