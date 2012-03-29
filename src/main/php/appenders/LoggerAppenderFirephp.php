c59666abb1a8e0d3c9ba92fab999290784e99f47
Ivan Habunek <ihabunek@apache.org>
2012-03-29 12:50:29 +0000

    LOG4PHP-174: Fixed implementation of default value for medium parameter.
    git-svn-id: https://svn.apache.org/repos/asf/logging/log4php/trunk@1306817 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/php/appenders/LoggerAppenderFirephp.php
+++ src/main/php/appenders/LoggerAppenderFirephp.php
@@ -24,7 +24,7 @@
  * This appender requires the FirePHP server library version 1.0 or later.
  * 
  * Configurable parameters of this appender are:
- * - medium - (string) The target to which messages will be sent. Possible options are 
+ * - mediummediutarget - (string) The target to which messages will be sent. Possible options are 
  *            'page' (default), 'request', 'package' and 'controller'. For more details,
  *            see FirePHP documentation.
  * 
@@ -61,7 +61,7 @@ class LoggerAppenderFirephp extends LoggerAppender {
 	 * The target for log messages. Possible values are: 'page' (default), 
 	 * 'request', 'package' and 'contoller'.
 	 */
-	protected $medium;
+	protected $medium = 'page';
 
 	public function activateOptions() {
 		$this->console = $this->getConsole();
@@ -108,7 +108,7 @@ class LoggerAppenderFirephp extends LoggerAppender {
 		
 		if (method_exists('FirePHP', 'to')) {
 			$inspector = FirePHP::to($this->getMedium());
-		
+			
 			return $inspector->console();
 		}
 		
@@ -121,7 +121,7 @@ class LoggerAppenderFirephp extends LoggerAppender {
 	}
 
 	/** Sets the medium. */
-	public function setMedium($medium = 'page') {
+	public function setMedium($medium) {
 		$this->setString('medium', $medium);
 	}
 }
