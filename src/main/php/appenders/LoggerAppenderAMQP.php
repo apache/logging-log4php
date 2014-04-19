119464ad621ee3d4691f18ab68c8015d369bd865
Dmitriy Ulyanov <dmitriy.ulyanov@wikimart.ru>
2014-04-19 21:55:39 +0400

    Changed @since in LoggerAppenderAMQP
    Signed-off-by: Ivan Habunek <ihabunek@apache.org>

--- src/main/php/appenders/LoggerAppenderAMQP.php
+++ src/main/php/appenders/LoggerAppenderAMQP.php
@@ -44,7 +44,7 @@
  *
  * @package log4php
  * @subpackage appenders
- * @since 3.0
+ * @since 2.4.0
  * @author Dmitry Ulyanov dmitriy.ulyanov@wikimart.ru
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
  * @link http://logging.apache.org/log4php/docs/appenders/amqp.html Appender documentation
