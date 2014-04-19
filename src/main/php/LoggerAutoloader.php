66e78c259dc2116fc201d17c638f0bae0a4837e9
Dmitriy Ulyanov <dmitriy.ulyanov@wikimart.ru>
2014-04-19 21:11:44 +0400

    Added LoggerLayoutGelf

--- src/main/php/LoggerAutoloader.php
+++ src/main/php/LoggerAutoloader.php
@@ -115,6 +115,7 @@ class LoggerAutoloader {
 		'LoggerPatternConverterThrowable' => '/pattern/LoggerPatternConverterThrowable.php',
 		
 		// Layouts
+		'LoggerLayoutGelf' => '/layouts/LoggerLayoutGelf.php',
 		'LoggerLayoutHtml' => '/layouts/LoggerLayoutHtml.php',
 		'LoggerLayoutPattern' => '/layouts/LoggerLayoutPattern.php',
 		'LoggerLayoutSerialized' => '/layouts/LoggerLayoutSerialized.php',
