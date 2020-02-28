d363521de4aa2cb26d5edb9854277708869df0bd
李枨煊 <i@chengxuan.li>
2020-02-28 10:51:21 +0800

    compatible php7.2
    The `each` function has been DEPRECATED as of PHP 7.2.0.  Replce by `foreach`

--- src/main/php/LoggerReflectionUtils.php
+++ src/main/php/LoggerReflectionUtils.php
@@ -68,7 +68,7 @@ class LoggerReflectionUtils {
 	public function setProperties($properties, $prefix) {
 		$len = strlen($prefix);
 		reset($properties);
-		while(list($key,) = each($properties)) {
+		foreach ($properties as $key => $value) {
 			if(strpos($key, $prefix) === 0) {
 				if(strpos($key, '.', ($len + 1)) > 0) {
 					continue;
